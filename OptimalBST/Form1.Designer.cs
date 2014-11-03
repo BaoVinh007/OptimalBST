2b8e2dbdeecfa3d230a6c3dad158a10d4c93a3d8
VinhNguyen <baovinh007@yahoo.com>
2014-11-03 00:04:40 -0800

    OptimalBST

---
+++ OptimalBST/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace OptimalBST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(150, 489);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(435, 22);
+            this.textBox3.TabIndex = 21;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 489);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 17);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Cost of optimal BST";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 515);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 17);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Order BST";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(53, 346);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(536, 137);
+            this.richTextBox2.TabIndex = 17;
+            this.richTextBox2.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 298);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 17);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "K-table";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(53, 159);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(536, 136);
+            this.richTextBox1.TabIndex = 15;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 17);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Cost of matrix";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(363, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(294, 22);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(275, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Enter sequence of n probabilities of nodes";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(53, 133);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(536, 22);
+            this.textBox5.TabIndex = 24;
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(16, 159);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(31, 136);
+            this.richTextBox3.TabIndex = 25;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(16, 346);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(31, 137);
+            this.richTextBox4.TabIndex = 27;
+            this.richTextBox4.Text = "";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(53, 318);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(536, 22);
+            this.textBox6.TabIndex = 26;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(16, 29);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(294, 22);
+            this.textBox4.TabIndex = 29;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 2);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 17);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Enter name of node";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Location = new System.Drawing.Point(49, 535);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(536, 119);
+            this.richTextBox5.TabIndex = 30;
+            this.richTextBox5.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 661);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "OptimalBST";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+    }
+}
+
---
+++ OptimalBST/Form1.cs
@@ -0,0 +1,205 @@
+﻿// Course: CS6423
+// Student name: Vinh Nguyen
+// Student ID: 000200899
+// Assignment #: #3
+// Due Date: 10/29/2013
+// Signature: ______________
+// (The signature means that the program is your own work)
+// Score: ______________
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OptimalBST
+{
+    public partial class Form1 : Form
+    {
+        // An array 2 dimensions contains cost of matrixes
+        public double[,] m = new double[100, 100];
+
+        // An array 2 dimensions contains probabilityies of key nodes
+        public double[,] prob = new double[100, 100];
+
+        // An array 2 dimensions contains k value
+        public int[,] s = new int[100, 100];
+
+        // A string shows order of multiplication matrixes
+        public string str;
+
+        // An array to be used to store name of nodes
+        public string[] strOutputBST = new string[100];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        //A function get string of subtrees  
+        private string construct_optimal_subtree(int i, int j, int r1, string subtree)
+        {
+            int t;
+            // a temporary string to get string of subtrees
+            string str1 = "";
+            if (i <= j)
+            {            
+                t = s[i,j];
+                str1 += strOutputBST[t] + " is " + subtree + " subtree of " + strOutputBST[r1] + "\n";
+                str1 += construct_optimal_subtree(i, t - 1, t, "left");
+                str1 += construct_optimal_subtree(t + 1, j, t, "right");
+            }
+            return str1;
+        } 
+
+        // A function to contructs binary tree. get root string 
+        private string construct_BST(int n)
+        {
+            int r1;
+            r1 = s[1,n];
+            str += strOutputBST[r1] + " is the root \n";
+            str += construct_optimal_subtree(1, r1 - 1, r1, "left");
+            str += construct_optimal_subtree(r1 + 1, n, r1, "right");            
+            return str;
+        } 
+        
+        // Optimal Binary Search Binary Tree Algorithm
+        private void optimalBST(double[] p, int n)
+        {
+            int j;
+            double q;
+            for (int i = 1; i <= n+1 ; i++)
+            {
+                m[i, i - 1] = 0;
+                prob[i, i - 1] = 0;            
+            }            
+            
+            for (int l = 1; l <= n ; l++)
+            {
+                // richtextbox1 is used to show cost table step by step
+                richTextBox1.Text += "Step"+l+":\n";
+                // richtextbox1 is used to show k table step by step
+                richTextBox2.Text += "Step" + l + ":\n";
+                for (int i = 1; i <= n - l + 1; i++)
+                {
+                    j = i + l - 1;
+                    m[i, j] = Math.Pow(10, 5);
+                    prob[i, j] = prob[i, j - 1] + p[j];
+                    for (int k = i; k <= j; k++)
+                    {
+                        q = m[i, k - 1] + m[k + 1, j] + prob[i, j];
+                        if (q < m[i, j])
+                        {
+                            m[i, j] = q;
+                            s[i, j] = k;
+                        }
+                    }
+                }
+                // Show the cost table
+                for (int i = 1; i <= n ; i++)
+                {
+                    for (int k = 1; k <= n ; k++)
+                    {
+                        richTextBox1.Text += m[i, k] + " \t";
+                        richTextBox1.Text += "  ";
+                    }
+                    richTextBox1.Text += "\n";
+                }
+                richTextBox1.Text += "\n";
+
+                // Show the k table
+                for (int i = 1; i <= n ; i++)
+                {
+                    for (int k = 1; k <= n ; k++)
+                    {
+                        richTextBox2.Text += s[i, k] + " \t";
+                        richTextBox2.Text += "  ";
+                    }
+                    richTextBox2.Text += "\n";
+                }
+                richTextBox2.Text += "\n";
+            }
+            richTextBox1.Text += "\n\n";
+            richTextBox2.Text += "\n\n";  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Reset all value = ""
+            if (richTextBox1.Text.ToString() != "")
+            {
+                richTextBox1.Text = "";
+                richTextBox2.Text = "";
+                richTextBox3.Text = "";
+                richTextBox4.Text = "";
+                richTextBox5.Text = "";                
+                textBox5.Text = "";
+                textBox6.Text = "";
+                str = "";
+            }
+
+            // Read an input string that contains name of node
+            string input1 = textBox4.Text.ToString();
+
+            // Split the input string  that contains name of node by white space
+            string[] arr1 = input1.Split(' ');
+
+            // a variable is assigned to store length of input                        
+            int n = 0;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] != "")
+                    n++;
+            }
+            
+            // An array stores all name of nodes
+            
+            for (int i = 0; i < n; i++)
+            {
+                strOutputBST[i+1] = arr1[i];                
+            }
+
+            // Read an input string that contains probabilities of nodes
+            string input = textBox1.Text.ToString();
+
+            // Split the input string that contains probabilities of nodes by white space
+            string[] arr = input.Split(' ');
+            
+            // a variable is assigned to store length of input            
+            n = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != "")
+                    n++;
+            }
+            // An array stores all probabilities of nodes
+            double[] p = new double[n+1];            
+            for (int i = 0; i < n; i++)
+            {
+                p[i+1] = Double.Parse(arr[i]);                
+            }
+            
+            for (int i = 0; i < n; i++)
+            {
+                textBox5.Text += i + "\t";
+                textBox6.Text += i + "\t";
+                richTextBox3.Text += i + 1 + "\n"; 
+                richTextBox4.Text += i + 1 + "\n";
+            }
+            //Call optimal binary search tree function
+            optimalBST(p, n);
+                        
+            // Show binary search tree
+            string strOrder = construct_BST(n);            
+            richTextBox5.Text = strOrder;
+
+            // Show cost of binary search tree
+            textBox3.Text = m[1, n].ToString();
+             
+        }
+    }
+}
